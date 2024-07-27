c502cb0d88f42a5651f554f04e6e289648b61f4f
ana117 <63540951+ana117@users.noreply.github.com>
2024-07-27 19:30:49 +0700

    Improve styling

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -6,19 +6,19 @@ import Link from "next/link";
 
 const Home: FC = () => {
     return (
-      <div className="grow p-8 flex">
-        <Card className="cursor-pointer max-w-1/3 h-fit hover:bg-slate-100/10 hover:scale-105 duration-500">
+      <div className="grow p-8 flex justify-center md:justify-between">
+        <Card className="cursor-pointer max-w-1/3 h-fit bg-secondary hover:scale-105 duration-500">
           <Link href="/stretch/starting">
-          <CardHeader>
-            <CardTitle>
-              Starting to Stretch
-            </CardTitle>
-          </CardHeader>
+            <CardHeader>
+              <CardTitle>
+                Starting to Stretch
+              </CardTitle>
+            </CardHeader>
             <CardContent className="flex flex-col gap-2">
               <p>
                 Full-body program for beginners from r/flexibility
               </p>
-              <div className="flex flex-col lg:flex-row justify-between gap-2">
+              <div className="hidden md:flex flex-col lg:flex-row justify-between gap-2">
                 <div>
                   <p>Upper Body</p>
                   <ul className="list-disc list-inside">
