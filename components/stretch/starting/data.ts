f89d407f9d323b01283dc4fe498aac670f5d0c64
ana117 <63540951+ana117@users.noreply.github.com>
2024-07-27 19:46:50 +0700

    Update lower body data

--- components/stretch/starting/data.ts
+++ components/stretch/starting/data.ts
@@ -22,8 +22,8 @@ const stretchExercises: StretchExercise[] = [
             'Slowly lift your arms up and away from your body, keeping your shoulders relaxed and your chest open.',
         ],
         repetitions: generateDefaultRepetitions(),
-     },
-     {
+    },
+    {
         id: 'spine-backbend',
         name: 'Spine Backbend',
         steps: [
@@ -32,8 +32,8 @@ const stretchExercises: StretchExercise[] = [
             'Lean back slightly while keeping your head neutral.',
         ],
         repetitions: generateDefaultRepetitions(),
-     },
-     {
+    },
+    {
         id: 'rear-hand-clasp',
         name: 'Rear Hand Clasp',
         steps: [
@@ -44,8 +44,8 @@ const stretchExercises: StretchExercise[] = [
             'Switch hands and repeat.',
         ],
         repetitions: generateDefaultRepetitions(),
-     },
-     {
+    },
+    {
         id: 'lying-cross',
         name: 'Lying Cross',
         steps: [
@@ -57,8 +57,8 @@ const stretchExercises: StretchExercise[] = [
         repetitions: [
             { type: RepetitionType.Hold, count: 30, text: 'Hold the stretch' },
         ],
-     },
-     {
+    },
+    {
         id: 'wrist-biceps-stretch',
         name: 'Wrist-Biceps Stretch',
         steps: [
@@ -67,7 +67,61 @@ const stretchExercises: StretchExercise[] = [
             'While keeping your arm straight, rotate your upper body away from the wall.',
         ],
         repetitions: generateDefaultRepetitions(),
-     }
+    },
+    {
+        id: 'one-leg-pike',
+        name: 'One Leg Pike',
+        steps: [
+            'Place a chair in front of you.',
+            'Stand on one leg and place the other leg on the chair.',
+            'Bend forward at the hips while keeping your back, arms, and leg straight.',
+            'Repeat on the other leg.',
+        ],
+        repetitions: generateDefaultRepetitions(),
+    },
+    {
+        id: 'kneeling-lunge',
+        name: 'Kneeling Lunge',
+        steps: [
+            'Put one knee on the ground and the other foot in front of you.',
+            'Move the leg in front of you forward slowly as far as you can.',
+            'Keep your back straight and upright.',
+            'Repeat on the other leg.',
+        ],
+        repetitions: generateDefaultRepetitions(),
+    },
+    {
+        id: 'pancake',
+        name: 'Pancake',
+        steps: [
+            'Sit on the floor with your legs spread wide.',
+            'Keep your back straight and your chest open.',
+            'Lean forward as far as you can while keeping your back straight.',
+        ],
+        repetitions: generateDefaultRepetitions(),
+    },
+    {
+        id: 'butterfly',
+        name: 'Butterfly',
+        steps: [
+            'Sit on the floor with the soles of your feet together.',
+            'Hold your feet with your hands.',
+            'Move your knees to the ground as far as you can.',
+            'If you can reach the ground with your knees, allow your body to lean forward.',
+        ],
+        repetitions: generateDefaultRepetitions(),
+    },
+    {
+        id: 'calf-stretch',
+        name: 'Calf Stretch',
+        steps: [
+            'Place a chair in front of you backwise.',
+            'Hold the chair with both hands at the backrest.',
+            'Place one foot back and try to keep the heel on the ground as far as you can.',
+            'Repeat on the other leg.',
+        ],
+        repetitions: generateDefaultRepetitions(),
+    }
 ];
 
 export default stretchExercises;
